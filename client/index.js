81176274d0136b902b7f8dea1e90347b768a5078
Lily Li <45389094+griffnut@users.noreply.github.com>
2019-03-18 16:26:44 -0400

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import {Provider} from 'react-redux'
-// import SoundKey from './components'
 import store from './store'
 import App from './app'
 
