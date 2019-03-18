24ba38f664f8ed31ab00ba2182b4c8ee2999adcd
Evgeniya Naumova <jainnaumova@gmail.com>
2019-03-18 16:50:15 -0400

    added babylon component

--- client/index.js
+++ client/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import {Provider} from 'react-redux'
-// import SoundKey from './components'
-import store from './store'
 import App from './app'
 
 // establishes socket connection
