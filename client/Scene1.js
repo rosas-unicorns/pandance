3ca0e0a5b879b88ad70de0a7c5426f7a3f95e51d
Farhuts <tfarguts@gmail.com>
2019-03-20 10:51:25 -0400

    added particles (#16)
    * added panda's arm movements
    
    * added particles

--- client/Scene1.js
+++ client/Scene1.js
@@ -24,8 +24,6 @@ const Scene1 = () => {
       )
       cameraArc.attachControl(canvas, true)
 
-      // camera.attachControl(canvas, true);
-
       //light environment light (comes from above)
       var light1 = new BABYLON.DirectionalLight(
         'light1',
@@ -69,10 +67,11 @@ const Scene1 = () => {
       BABYLON.SceneLoader.ImportMesh(
         '',
         '/assets/',
-        'panda2 (1).babylon',
+        'panda.babylon',
         scene,
         function(newMeshes, particleSystems, skeletons) {
           var skeleton = skeletons[0]
+          var panda = newMeshes[0]
 
           // POSES
           var defaultPose = scene.beginWeightedAnimation(
@@ -85,23 +84,65 @@ const Scene1 = () => {
           var bothArmsUp = scene.beginWeightedAnimation(skeleton, 1, 2, 0, true)
           var leftLegUp = scene.beginWeightedAnimation(skeleton, 4, 5, 0, true)
           var lastAnim = defaultPose
-          //
-          Mousetrap.bind('q', () => {
-            lastAnim.syncWith(bothArmsUp)
-            bothArmsUp.syncWith()
-            let obs = scene.onBeforeAnimationsObservable.add(function() {
-              lastAnim.weight -= 0.1
-              if (lastAnim.weight <= 0) {
-                scene.onBeforeAnimationsObservable.remove(obs)
-                lastAnim.weight = 0
-                bothArmsUp.weight = 1.0
 
-                lastAnim = bothArmsUp
-              } else {
-                bothArmsUp.weight = 1.0 - lastAnim.weight
-              }
-            })
-          })
+          particleSystem.emitter = panda
+          particleSystem.minEmitPanda = new BABYLON.Vector3(-1, 0, 0)
+          particleSystem.maxEmitPanda = new BABYLON.Vector3(1, 0, 0)
+
+          particleSystem.color1 = new BABYLON.Color4(0, 0, 0, 10)
+          // particleSystem.color2 = new BABYLON.Color4(0.2, 0.5, 1.0, 1.0);
+          // particleSystem.colorDead = new BABYLON.Color4(0, 0, 0.2, 0.0);
+
+          particleSystem.minSize = 0.1
+          particleSystem.maxSize = 0.5
+
+          // Life time of each particle (random between...
+          particleSystem.minLifeTime = 0.3
+          particleSystem.maxLifeTime = 1.5
+
+          // Emission rate
+          particleSystem.emitRate = 800
+
+          // Set the gravity of all particles
+          particleSystem.gravity = new BABYLON.Vector3(0, -9.81, 0)
+
+          // Direction of each particle after it has been emitted
+          particleSystem.direction1 = new BABYLON.Vector3(-7, 8, 3)
+          particleSystem.direction2 = new BABYLON.Vector3(7, 8, -3)
+
+          // Angular speed, in radians
+          particleSystem.minAngularSpeed = 0
+          particleSystem.maxAngularSpeed = Math.PI
+
+          // Speed
+          particleSystem.minEmitPower = 1
+          particleSystem.maxEmitPower = 3
+          particleSystem.updateSpeed = 0.005
+
+          Mousetrap.bind(
+            'q',
+            () => {
+              // Start the particle system
+              particleSystem.start()
+              setTimeout(() => particleSystem.stop(), 500)
+
+              lastAnim.syncWith(bothArmsUp)
+              bothArmsUp.syncWith()
+              let obs = scene.onBeforeAnimationsObservable.add(function() {
+                lastAnim.weight -= 0.1
+                if (lastAnim.weight <= 0) {
+                  scene.onBeforeAnimationsObservable.remove(obs)
+                  lastAnim.weight = 0
+                  bothArmsUp.weight = 1.0
+
+                  lastAnim = bothArmsUp
+                } else {
+                  bothArmsUp.weight = 1.0 - lastAnim.weight
+                }
+              })
+            },
+            'keyup'
+          )
 
           Mousetrap.bind('w', () => {
             lastAnim.syncWith(leftLegUp)
@@ -118,7 +159,7 @@ const Scene1 = () => {
                 leftLegUp.weight = 1.0 - lastAnim.weight
               }
             })
-          })
+          }, 'keyup')
         }
       )
 
