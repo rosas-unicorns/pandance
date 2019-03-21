1dfe216894f02633768d5828432d23ee766424f7
Tetiana Farhuts <tfarguts@gmail.com>
2019-03-21 13:26:38 -0400

    changed and added a new keyboard sound

--- client/SoundKey.js
+++ client/SoundKey.js
@@ -20,61 +20,163 @@ class SoundKey extends Component {
   componentDidMount() {
     Mousetrap.bind('q', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav'
+      audio.src = 'assets/music/bubbles.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('w', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/026/cymbal1.wav'
+      audio.src = 'assets/music/clay.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('e', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/015/dino-baby.wav'
+      audio.src = 'assets/music/dotted-spiral.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('y', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav'
+      audio.src = 'assets/music/glimmer.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('t', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav'
+      audio.src = 'assets/music/moon.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('r', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav'
+      audio.src = 'assets/music/moon2.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('u', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav'
+      audio.src = 'assets/music/pinwheel.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('i', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav'
+      audio.src = 'assets/music/pinwheel2.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('o', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav'
+      audio.src = 'assets/music/pinwheel3.mp3'
       audio.load()
       audio.play()
     })
     Mousetrap.bind('p', () => {
       const audio = document.createElement('audio')
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav'
+      audio.src = 'assets/music/piston-1.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('a', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/piston-2.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('s', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/piston-3.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('d', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/prism-1.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('f', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/prism-2.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('g', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/splits.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('h', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/squiggle.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('j', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/suspension.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('k', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/timer.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('l', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/wipe.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('z', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/wipe2.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('x', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/veil.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('c', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/confetti2.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('v', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/flash-3.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('b', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/glimmer2.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('n', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/time2.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('m', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/zig-zag.mp3'
+      audio.load()
+      audio.play()
+    })
+    Mousetrap.bind('space', () => {
+      const audio = document.createElement('audio')
+      audio.src = 'assets/music/clap-loop.mp3'
       audio.load()
       audio.play()
     })
