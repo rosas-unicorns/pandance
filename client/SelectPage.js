febc3fe750d730ea5556b5a52e4e24434d2a3c99
Lily Li <lily.li0597@gmail.com>
2019-03-22 11:27:36 -0400

    beautified panda scene

---
+++ client/SelectPage.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react'
+import Pandance from './Pandance'
+
+export default class Selection extends Component {
+  constructor() {
+    super()
+    this.state = {
+      play: false,
+      background: 'space',
+      character: 'panda'
+    }
+
+    this.selectBackground = this.selectBackground.bind(this)
+    this.selectCharacter = this.selectCharacter.bind(this)
+    this.readyToPlay = this.readyToPlay.bind(this)
+  }
+
+  selectBackground(e) {
+    this.setState({
+      background: e.target.name
+    })
+  }
+
+  selectCharacter(e) {
+    this.setState({
+      character: e.target.name
+    })
+  }
+
+  readyToPlay() {
+    this.setState({
+      play: true
+    })
+  }
+
+  render() {
+    const backgrounds = ['space', 'bubbles']
+    const characterName = ['panda']
+    const scenes = [
+      <Pandance
+        character={this.state.character}
+        background={this.state.background}
+      />
+    ]
+
+    return (
+      <div>
+        {!this.state.play ? (
+          <div>
+            <h1>Select A Scene</h1>
+            {backgrounds.map((background, idx) => {
+              return (
+                <button
+                  type="button"
+                  key={background}
+                  name={background}
+                  onClick={this.selectBackground}
+                >
+                  {background}
+                </button>
+              )
+            })}
+            <h1>Select A Character</h1>
+            {characterName.map((character, idx) => {
+              return (
+                <button
+                  type="button"
+                  key={character}
+                  name={character}
+                  onClick={this.selectCharacter}
+                >
+                  {character}
+                </button>
+              )
+            })}
+            <button type="button" onClick={this.readyToPlay}>
+              Play
+            </button>
+          </div>
+        ) : (
+          <div>{scenes[0]}</div>
+        )}
+      </div>
+    )
+  }
+}
